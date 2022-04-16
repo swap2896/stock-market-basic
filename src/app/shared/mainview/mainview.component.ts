810912daccc1b40840e37f8eadce9646553beac5
swap2896 <kalra.swapnill2896@gmail.com>
2022-04-16 19:38:33 +0530

    fixed company list table

--- src/app/shared/mainview/mainview.component.ts
+++ src/app/shared/mainview/mainview.component.ts
@@ -75,7 +75,8 @@ export class MainviewComponent implements OnInit {
   
   ngOnInit(): void {
     this.isloading=true;
-    this.httpclient.get('assets/earnings_calendar.csv', {responseType: 'text'})
+    //for localhost try assets/earnings_calendar.csv
+    this.httpclient.get('https://github.com/swap2896/stock-market-basic/blob/main/src/assets/earnings_calendar.csv', {responseType: 'text'})
     .subscribe(
         (data:any) => {
             let result=data.split('\r\n');
